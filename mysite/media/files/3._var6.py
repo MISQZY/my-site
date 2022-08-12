b817005ffad17923e3f3b451e4012bf9c8d7c659
MISQZY <npershutov@gmail.com>
2022-08-12 19:11:53 +0300

    remove media folder

--- mysite/media/files/3._var6.py
+++
@@ -1,8 +0,0 @@
-def main(a, n, z):
-    r = 0
-    for k in range(1, n+1):
-        for j in range(1, a+1):
-            r += pow((61 * z * z * z - j * j / 25 - j), 6) +\
-                 pow((20 * k - 1), 5)
-
-    return r
--- mysite/media/files/5._var6.py
+++
@@ -1,9 +0,0 @@
-import math
-
-def main(x, y):
-    s = 0
-    n = len(x)
-    for i in range(len(x)):
-        s += y[i // 2] ** 3 - 10 * x[n - 1 - i] - y[i // 2] ** 2 / 17
-
-    return s
